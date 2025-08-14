0681f6066e0507c9b2e7b5543a1e77f7a5815477
Phil <phil@logphile.com>
2025-08-14 16:49:40 -0400

    fix: remove page transition to isolate error

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
     <DevErrorBoundary>
-      <NuxtPage :transition="{ name: 'page', mode: 'out-in' }" />
+      <NuxtPage />
     </DevErrorBoundary>
   </NuxtLayout>
 </template>
