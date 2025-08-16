e66eeaa7cf2c99fb2acc0e47bd608f2225ba0109
Phil <phil@logphile.com>
2025-08-16 15:16:37 -0400

    fix various issues

--- app/components/DevDock.client.vue
+++ app/components/DevDock.client.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const SHOW_DEBUG = import.meta.env.DEV || import.meta.env.VITE_DEBUG_TOOLS === '1'
 const cfg = useRuntimeConfig().public
 const route = useRoute()
 const query = useSafeQuery()
@@ -7,11 +8,11 @@ let store: any = null; try { store = useProfileStore() } catch {}
 </script>
 
 <template>
-  <div class="fixed top-2 right-2 z-[9998] bg-black/80 text-white text-xs rounded p-2">
+  <div v-if="SHOW_DEBUG" class="fixed top-2 right-2 z-[9998] bg-black/80 text-white text-xs rounded p-2 pointer-events-none">
     <div>route: {{ route.fullPath }}</div>
     <div>query: {{ query }}</div>
     <div>supabase.url set: {{ !!cfg?.supabase?.url }}</div>
-    <div>user: {{ user?.value ? (user.value.email || user.value.id) : 'null' }}</div>
+    <div>user: {{ user ? (user.email || user.id) : 'null' }}</div>
     <div>store: {{ !!store }}</div>
     <div>store.keys: {{ store ? Object.keys(store) : 'none' }}</div>
   </div>
